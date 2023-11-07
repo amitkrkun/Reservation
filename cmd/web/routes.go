657d327ec8d3b471b7445661b5ce1ea37c4f6aa3
amitkrsh.1999@gmail.com <amitkrsharma>
2023-11-07 22:02:44 +0530

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/amitkrkun/booking/pkg/config"
+	"github.com/amitkrkun/booking/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+
+	// mux := pat.New()
+
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Use(NoSruf)
+
+	mux.Use(SessionLoad)
+	//	mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+
+}
